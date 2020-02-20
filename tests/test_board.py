f3ec5795c51c4afd161a47bff7b4daa674b1df44
Jakub Komárek <komaja@email.cz>
2020-02-20 19:03:29 +0100

    Fixed board unit tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,4 +1,5 @@
 from models.board import BoardModel
+from models.constants import Direction
 from models.tile import TileModel
 import unittest as ut
 
@@ -7,19 +8,18 @@ class TestPlace(ut.TestCase):
     def setUp(self):
         self.board = BoardModel(15)
 
-    def test_first(self):
-        self.board.place(0, 0)
-        self.assertEqual(TileModel.Symbols.CROSS, self.board[0][0].symbol.get())
+    def test_circle(self):
+        self.board.place(0, 0, TileModel.Symbols.CIRCLE)
+        self.assertEqual(TileModel.Symbols.CIRCLE, self.board[0][0].symbol.get())
 
-    def test_second(self):
-        self.board.place(0, 0)
-        self.board.place(0, 1)
-        self.assertEqual(TileModel.Symbols.CIRCLE, self.board[0][1].symbol.get())
+    def test_cross(self):
+        self.board.place(1, 0, TileModel.Symbols.CROSS)
+        self.assertEqual(TileModel.Symbols.CROSS, self.board[1][0].symbol.get())
 
     def test_invalid(self):
-        self.board.place(0, 0)
-        self.board.place(0, 0)
-        self.assertEqual(TileModel.Symbols.CROSS, self.board[0][0].symbol.get())
+        self.board.place(0, 0, TileModel.Symbols.CIRCLE)
+        self.board.place(0, 0, TileModel.Symbols.CROSS)
+        self.assertEqual(TileModel.Symbols.CIRCLE, self.board[0][0].symbol.get())
 
 
 class TestNextTile(ut.TestCase):
@@ -27,34 +27,34 @@ class TestNextTile(ut.TestCase):
         self.board = BoardModel(15)
 
     def test_horizontal_first(self):
-        self.assertEqual((0, 1), self.board.next_tile(0, 0, BoardModel.Direction.HORIZONTAL))
+        self.assertEqual((0, 1), self.board.next_tile(0, 0, Direction.HORIZONTAL))
 
     def test_horizontal_one_but_last(self):
-        self.assertEqual((14, 14), self.board.next_tile(14, 13, BoardModel.Direction.HORIZONTAL))
+        self.assertEqual((14, 14), self.board.next_tile(14, 13, Direction.HORIZONTAL))
 
     def test_vertical_first(self):
-        self.assertEqual((1, 0), self.board.next_tile(0, 0, BoardModel.Direction.VERTICAL))
+        self.assertEqual((1, 0), self.board.next_tile(0, 0, Direction.VERTICAL))
 
     def test_vertical_one_but_last(self):
-        self.assertEqual((14, 14), self.board.next_tile(13, 14, BoardModel.Direction.VERTICAL))
+        self.assertEqual((14, 14), self.board.next_tile(13, 14, Direction.VERTICAL))
 
     def test_diagonal_a_first(self):
-        self.assertEqual((1, 0), self.board.next_tile(0, 1, BoardModel.Direction.DIAGONAL_A))
+        self.assertEqual((1, 0), self.board.next_tile(0, 1, Direction.DIAGONAL_A))
 
     def test_diagonal_a_one_but_last(self):
-        self.assertEqual((14, 13), self.board.next_tile(13, 14, BoardModel.Direction.DIAGONAL_A))
+        self.assertEqual((14, 13), self.board.next_tile(13, 14, Direction.DIAGONAL_A))
 
     def test_diagonal_a_center(self):
-        self.assertEqual((14, 0), self.board.next_tile(13, 1, BoardModel.Direction.DIAGONAL_A))
+        self.assertEqual((14, 0), self.board.next_tile(13, 1, Direction.DIAGONAL_A))
 
     def test_diagonal_b_first(self):
-        self.assertEqual((14, 1), self.board.next_tile(13, 0, BoardModel.Direction.DIAGONAL_B))
+        self.assertEqual((14, 1), self.board.next_tile(13, 0, Direction.DIAGONAL_B))
 
     def test_diagonal_b_one_but_last(self):
-        self.assertEqual((1, 14), self.board.next_tile(0, 13, BoardModel.Direction.DIAGONAL_B))
+        self.assertEqual((1, 14), self.board.next_tile(0, 13, Direction.DIAGONAL_B))
 
     def test_diagonal_b_center(self):
-        self.assertEqual((14, 14), self.board.next_tile(13, 13, BoardModel.Direction.DIAGONAL_B))
+        self.assertEqual((14, 14), self.board.next_tile(13, 13, Direction.DIAGONAL_B))
 
 
 if __name__ == "__main__":
