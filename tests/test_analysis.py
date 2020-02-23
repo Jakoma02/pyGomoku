f6a98b8d0aafc213235b1babb6291c1129acd2d1
Jakub Komárek <komaja@email.cz>
2020-02-23 19:12:37 +0100

    Improved analysis test
    Added new test
    Fixed assertion order

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -17,13 +17,13 @@ class TestCounting(unittest.TestCase):
         self.board.place(0, 0, TileModel.Symbols.CIRCLE)
         self.board.place(0, 1, TileModel.Symbols.CIRCLE)
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((0, 1), doubles)
+        self.assertEqual(doubles, (0, 1))
 
     def test_cross(self):
         self.board.place(0, 0, TileModel.Symbols.CROSS)
         self.board.place(0, 1, TileModel.Symbols.CROSS)
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((1, 0), doubles)
+        self.assertEqual(doubles, (1, 0))
 
     def test_both(self):
         self.board.place(0, 0, TileModel.Symbols.CIRCLE)
@@ -32,7 +32,7 @@ class TestCounting(unittest.TestCase):
         self.board.place(2, 0, TileModel.Symbols.CROSS)
 
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((1, 1), doubles)
+        self.assertEqual(doubles, (1, 1))
 
     def test_longer(self):
         self.board.place(0, 0, TileModel.Symbols.CIRCLE)
@@ -40,29 +40,35 @@ class TestCounting(unittest.TestCase):
         self.board.place(2, 0, TileModel.Symbols.CIRCLE)
 
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((0, 0), doubles)
+        self.assertEqual(doubles, (0, 0))
 
     def test_last_symbol(self):
         pos = [(0, 13), (0, 14)]
         self._place_multiple(TileModel.Symbols.CIRCLE, pos)
 
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((0, 1), doubles)
+        self.assertEqual(doubles, (0, 1))
 
     def test_vertical(self):
         pos = [(0, 0), (1, 0)]
         self._place_multiple(TileModel.Symbols.CIRCLE, pos)
 
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((0, 1), doubles)
+        self.assertEqual(doubles, (0, 1))
 
     def test_diagonal(self):
         pos = [(0, 0), (1, 1)]
         self._place_multiple(TileModel.Symbols.CIRCLE, pos)
 
         doubles = analysis.n_tet_counts(self.board, 2)
-        self.assertEqual((0, 1), doubles)
+        self.assertEqual(doubles, (0, 1))
 
+    def test_more_in_line(self):
+        pos = [(0, 0), (0, 1), (0, 2), (0, 4), (0, 5), (0, 6)]
+        self._place_multiple(TileModel.Symbols.CIRCLE, pos)
+
+        doubles = analysis.n_tet_counts(self.board, 3)
+        self.assertEqual(doubles, (0, 2))
 
 if __name__ == '__main__':
     unittest.main()
