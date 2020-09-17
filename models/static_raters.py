3b5aa4b7880ee046d391b1e01aa084657a7ebada
Jakub Komárek <komaja@email.cz>
2020-09-17 12:51:22 +0200

    Add RandomRate:

--- models/static_raters.py
+++ models/static_raters.py
@@ -1,1 +1,26 @@
-from models.tile import TileModelfrom .analysis import n_tet_countsclass SimpleRater:    def rate(self, board):        COUPLE_COST = 5        TRIPLET_COST = 20        QUARTET_COST = 50        WIN_COST = 99999        couple_counts = n_tet_counts(board, 2)        triplet_counts = n_tet_counts(board, 3)        quartet_counts = n_tet_counts(board, 4)        win_counts = n_tet_counts(board, 5)        return (couple_counts[0] - couple_counts[1]) * COUPLE_COST + \               (triplet_counts[0] - triplet_counts[1]) * TRIPLET_COST + \               (quartet_counts[0] - quartet_counts[1]) * QUARTET_COST + \               (win_counts[0] - win_counts[1]) * WIN_COST
+from models.tile import TileModel
+from .analysis import n_tet_counts
+from random import randint
+
+class RandomRater:
+    def rate(self, board):
+        return randint(1, 100)
+
+class SimpleRater:
+    def rate(self, board):
+        COUPLE_COST = 5
+        TRIPLET_COST = 20
+        QUARTET_COST = 50
+        WIN_COST = 99999
+
+        couple_counts = n_tet_counts(board, 2)
+        triplet_counts = n_tet_counts(board, 3)
+        quartet_counts = n_tet_counts(board, 4)
+        win_counts = n_tet_counts(board, 5)
+
+        rating = (couple_counts[0] - couple_counts[1]) * COUPLE_COST + \
+                 (triplet_counts[0] - triplet_counts[1]) * TRIPLET_COST + \
+                 (quartet_counts[0] - quartet_counts[1]) * QUARTET_COST + \
+                 (win_counts[0] - win_counts[1]) * WIN_COST
+
+        return rating
